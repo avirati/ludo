ccc4153b6bb5a881123b284db8925e552aa1129e
Avinash Verma <avinash.verma@oradian.com>
2020-05-19 17:31:04 +0200

    Add coins

---
+++ src/containers/Base/components/Coin.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { getStyleObject } from 'containers/utils';
+import { COIN_SIZE } from 'globalConstants';
+import { BaseColors } from 'state/interfaces';
+
+import styles from './Coin.module.css';
+
+interface IProps {
+  baseColor: BaseColors;
+}
+
+export class Coin extends React.PureComponent<IProps> {
+  render() {
+    return (
+      <div className={styles.Container} style={getStyleObject(COIN_SIZE * 0.8, COIN_SIZE * 0.8, this.props.baseColor)}/>
+    );
+  }
+}
--- src/containers/Base/components/CoinPlaceholder.tsx
+++ src/containers/Base/components/CoinPlaceholder.tsx
@@ -4,6 +4,8 @@ import { getStyleObject } from 'containers/utils';
 import { COIN_PLACEHOLDER_SIZE, COIN_SIZE } from 'globalConstants';
 import { BaseColors } from 'state/interfaces';
 
+import { Coin } from './Coin';
+
 import styles from './CoinPlaceholder.module.css';
 
 interface ICoinPlaceholderProps {
@@ -15,7 +17,9 @@ export class CoinPlaceholder extends React.PureComponent<ICoinPlaceholderProps> 
     const { baseColor } = this.props;
     return (
       <div className={styles.Container} style={getStyleObject(COIN_PLACEHOLDER_SIZE, COIN_PLACEHOLDER_SIZE)}>
-        <div className={styles.Circle} style={getStyleObject(COIN_SIZE, COIN_SIZE, baseColor)}/>
+        <div className={styles.Circle} style={getStyleObject(COIN_SIZE, COIN_SIZE, baseColor)}>
+          <Coin baseColor={baseColor}/>
+        </div>
       </div>
     );
   }
